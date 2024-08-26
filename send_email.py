4f7a6584af2b7dec53486379bcbb57986928485a
chavdajay415 <chavdajay415@gmail.com>
2024-08-26 10:11:22 +0530

    Delete send_email.py

--- send_email.py
+++
@@ -1,8 +0,0 @@
-# Define current assets and current liabilities
-current_assets = 1000
-current_liabilities = 1000
-
-# Calculate the current ratio
-current_ratio = current_assets / current_liabilities
-
-print(f"The current ratio is: {current_ratio}")
